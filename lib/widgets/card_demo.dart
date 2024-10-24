44d6120822f39d89909ef168ff056b6e1b61901f
patipan-patisampita <patipan-patisampita@gmail.com>
2024-10-24 17:05:58 +0700

    update

--- lib/widgets/card_demo.dart
+++ lib/widgets/card_demo.dart
@@ -8,9 +8,28 @@ class CardDemo extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
-        title: const Text("Row widget"),
+        title: const Text("Card View"),
       ),
-      body: Card(),
+      body: Center(
+        child: Card(
+          child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: () {
+              debugPrint("CARD-1");
+            },
+            child: SizedBox(
+              width: 300,
+              height: 100,
+              child: const Column(
+                children: [
+                  Icon(Icons.home),
+                  Text("CARD-1")
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
